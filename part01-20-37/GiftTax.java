683a9db7c0cc4b7fd4fd8bec55a4335aadd25e14
eirinhagen <142972367+sunwontrise@users.noreply.github.com>
2024-02-25 11:13:12 +0100

    Leap year and Gift Tax exercises

--- part01-20-37/GiftTax.java
+++ part01-20-37/GiftTax.java
@@ -1,3 +1,28 @@
+import java.util.Scanner;
+
 public class GiftTax {
-    //I´ll do this later
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Value of gift?");
+        int gift = Integer.parseInt(scanner.nextLine());
+        double tax = 0;
+        if (gift < 5000) {
+            System.out.println("No tax!");
+        } else if (gift >= 5000 && gift < 25000) {
+            tax = 100 + (gift - 5000) * 0.08;
+            System.out.println("Your tax is:" + tax);
+        } else if (gift >= 25000 && gift < 55000) {
+            tax = 1700 + (gift - 25000) * 0.1;
+            System.out.println("Your tax is:" + tax);
+        } else if (gift >= 55000 && gift < 200000) {
+            tax = 4700 + (gift - 55000) * 0.12;
+            System.out.println("Your tax is:" + tax);
+        } else if (gift >= 200000 && gift < 1000000) {
+            tax = 22100 + (gift - 200000) * 0.15;
+            System.out.println("Your tax is:" + tax);
+        } else if (gift >= 100000) {
+            tax = 142100 + (gift - 1000000) * 0.17;
+            System.out.println("Your tax is:" + tax);
+        }
+    }
 }
--- part01-20-37/LeapYear.java
+++ part01-20-37/LeapYear.java
@@ -1,15 +1,20 @@
 import java.util.Scanner;
+
 public class LeapYear {
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        System.out.println("Check if year is leap");
-        int year = Integer.parseInt(scanner.nextLine());
-        if (year % 100 == 0 && year % 400 == 0){
-            System.out.println("Year is leap!");
-        } else if (year % 4 == 0) {
-            System.out.println("Year is leap(2)!");
-        }else{
-            System.out.println("Year is not leap!");
+        Scanner scan = new Scanner(System.in);
+
+        System.out.println("Give a year:");
+        int year = Integer.parseInt(scan.nextLine());
+
+        if (year % 4 != 0) {
+            System.out.println("The year is not a leap year.");
+        } else if (year % 100 == 0 && year % 400 == 0) {
+            System.out.println("The year is a leap year");
+        } else if (year % 100 == 0 && year % 400 != 0) {
+            System.out.println("The year is not a leap year.");
+        } else {
+            System.out.println("The year is a leap year.");
         }
     }
 }
